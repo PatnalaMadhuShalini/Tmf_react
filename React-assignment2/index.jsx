0847cee1f85916315114b1346aa6724a74b0dbbd
git <madhushalinipatnala@gmail.com>
2025-11-24 23:41:41 +0530

    update

--- React-assignment2/index.jsx
+++ React-assignment2/index.jsx
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const heading = React.createElement("h1", null, "Hello World from React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading = React.createElement("h1", null, "Hello World from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+>>>>>>> 2ca189597993e1e663fa93f2f7d93f03093de1ae
